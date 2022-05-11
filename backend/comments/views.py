a787930cb80af70e6b7f76c0cb4486ddc6119350
andrew-s-cole <andrew.s.cole@gmail.com>
2022-05-11 18:00:17 -0400

    Created GET And POST Views For Comments

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shotcuts import get_object_or_404
+from rest_framework import status
+from rest_framework import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request, id):
+    comments = Comment.objects.filter(video_id=id)
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_comment(request):
+    'User ', f"{request.user.id} {request.user.email} {request.user.username}"
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
