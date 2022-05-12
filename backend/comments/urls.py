1f50c314e69310964d9decf8d86fb6ad318c4911
andrew-s-cole <andrew.s.cole@gmail.com>
2022-05-12 15:41:48 -0400

    Updated Comments Views + Updated Admin File

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_comments),
-    path('<str:video_id>/', views.user_comments),
+    path('<id>/', views.get_all_comments),
+    path('<id>/', views.user_comments),
     path('alter/<int:pk>/', views.alter_comments),
 
 ]
