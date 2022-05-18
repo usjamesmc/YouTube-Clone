a4b7d2ecb22b6416d28cfab2fc1febf122b5946e
usjamesmc <usjamesmc@gmail.com>
2022-05-18 10:50:04 -0500

    Search button now takes you to search page

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import useCustomForm from "../../hooks/useCustomForm";
+import { useNavigate, Link } from "react-router-dom";
+
+
 
 
 const VideoSearch = (props) => {
 
-    // const [search, setSearch] = useState('');
+    const [search, setSearch] = useState('');
+    const [formData, handleInputChange, handleSubmit] = useCustomForm()
+    const navigate = useNavigate();
    
     return(
-        <div className='border-box'>
-            <form>
-            <div>
-                <label>Search</label>
-            </div>
-            <div>
-                <input type='text'/>
-            </div>
-            <div>
-                <button type='submit'>Search</button>
-                <Link to="/search">Click to register!</Link>
-            </div>
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <label>
+                Search{" "}
+                <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                />
+                </label>
+                <button onClick={() => navigate("/search")}>Search</button>
             </form>
         </div>
     );
